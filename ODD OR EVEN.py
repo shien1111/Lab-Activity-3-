b1639d4c0810685a7901fb32640482a726dc9926
shien1111 <shienly25@gmail.com>
2025-03-09 15:36:20 +0800

    Add files via upload

---
+++ ODD OR EVEN.py
@@ -0,0 +1,9 @@
+def check_number(num):
+    if num % 2 == 0:
+        print(f"{num} is an even number.")
+    else:
+        print(f"{num} is an odd number.")
+        
+check_number(49)
+check_number(87)
+check_number(22)
